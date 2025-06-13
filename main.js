87c7616369e9e4f71cf713aca1a4bcfe34272005
ksg0826 <liveok2013@gmail.com>
2025-06-13 15:21:06 +0900

    test

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 // main.js
 
 App.showCenterLabel("Hello world");
+
+//UI가 노출될 위치(정렬, 가로, 세로) 값을 변수로 사전에 제작
+let postion = 'middle';
+let width = 600;
+let height = 600;
+
+// my.html로 state라는 태그를 만들어 hello라는 값을 전달
+let _widget = App.showWidget('my.html', position, width, height);
+_widget.sendMessage({
+	state: "hello guys",
+}); 
